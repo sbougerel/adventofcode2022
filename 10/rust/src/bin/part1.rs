2d71f693340ce514a664d8d8f6ee40998908e143
Sylvain Bougerel <sylvain.bougerel@gmail.com>
2022-12-14 03:00:53 +0800

    Completed day 10 in Rust

--- 10/rust/src/bin/part1.rs
+++ 10/rust/src/bin/part1.rs
@@ -1,3 +1,47 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let sig_inter = 40;
+    let mut sig_peek = 20;
+    let mut sig_sum = 0;
+
+    let mut reg_x = 1;
+    let mut str_x = reg_x;
+    let mut cycle = 1;
+    let mut busy_cycles = 0;
+
+    loop {
+        if busy_cycles == 0 {
+            reg_x = str_x;
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            if input.is_empty() {
+                break;
+            }
+            let tokens = input.split(" ").collect::<Vec<&str>>();
+            match tokens[0].trim() {
+                "noop" => {
+                    busy_cycles = 1;
+                    str_x = reg_x;
+                }
+                "addx" => {
+                    busy_cycles = 2;
+                    str_x = reg_x + tokens[1].trim().parse::<i32>().unwrap();
+                }
+                &_ => {
+                    todo!();
+                }
+            }
+        }
+
+        if cycle == sig_peek {
+            sig_peek += sig_inter;
+            sig_sum += reg_x * cycle;
+        }
+
+        cycle += 1;
+        busy_cycles -= 1;
+    }
+
+    println!("{sig_sum}");
 }
--- 10/rust/src/bin/part2.rs
+++ 10/rust/src/bin/part2.rs
@@ -1,3 +1,48 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let line = 40;
+    let mut pos = 0;
+
+    let mut reg_x = 1;
+    let mut str_x = reg_x;
+    let mut busy_cycles = 0;
+
+    loop {
+        if busy_cycles == 0 {
+            reg_x = str_x;
+
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            if input.is_empty() {
+                break;
+            }
+            let tokens = input.split(" ").collect::<Vec<&str>>();
+
+            match tokens[0].trim() {
+                "noop" => {
+                    busy_cycles = 1;
+                    str_x = reg_x;
+                }
+                "addx" => {
+                    busy_cycles = 2;
+                    str_x = reg_x + tokens[1].trim().parse::<i32>().unwrap();
+                }
+                &_ => todo!(),
+            }
+        }
+
+        if pos >= reg_x - 1 && pos <= reg_x + 1 {
+            print!("#");
+        } else {
+            print!(".");
+        }
+        pos += 1;
+        if pos >= line {
+            println!("");
+            pos = 0;
+        }
+
+        busy_cycles -= 1;
+    }
 }
